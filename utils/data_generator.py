fix error that made gradients non existant

utils/data_generator.py
@@ -12,12 +12,12 @@ def data_generator(captions_vector, features_dict, num_images_per_batch, vocabul
             num_stored_images += 1
             photo = features_dict[key]
             for i in range(1, len(sequence)):
-                if sequence[i + 1] == 0:
+                if (i + 1 == len(sequence)) or sequence[i + 1] == 0:
                     break
                 # split into input and output pair
                 in_seq, out_seq = sequence[:i], sequence[i]
                 # pad input sequence
-                in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+                in_seq = pad_sequences([in_seq], maxlen=max_length, padding='post')[0]
                 # encode output sequence
                 out_seq = to_categorical([out_seq], num_classes=vocabulary_size)[0]
                 # store
@@ -27,6 +27,6 @@ def data_generator(captions_vector, features_dict, num_images_per_batch, vocabul
 
                 # yield the batch data
                 if num_stored_images == num_images_per_batch:
-                    yield [[np.array(features), np.array(partial_captions)], np.array(next_word)]
+                    yield (np.array(features), np.array(partial_captions)),  np.array(next_word)
                     features, partial_captions, next_word = [], [], []
                     num_stored_images = 0